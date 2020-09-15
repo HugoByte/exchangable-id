a79a2ebc155596b5e05eaf0dac2fc5fef5a2504d
Muhammed Irfan K <muhammedirfan@hugobyte.com>
2020-09-15 16:46:03 +0530

    Updated Linked Item

--- src/linked_item.rs
+++ src/linked_item.rs
@@ -1,20 +1,20 @@
-use frame_support::{StorageMap, Parameter};
-use sp_runtime::traits::Member;
-use codec::{Encode, Decode};
+use support::{StorageMap, Parameter};
+use runtime_primitives::traits::Member;
+use parity_codec::{Encode, Decode};
 
 #[cfg_attr(feature = "std", derive(Debug, PartialEq, Eq))]
 #[derive(Encode, Decode)]
-pub struct LinkedItem<Value> {
-	pub prev: Option<Value>,
-	pub next: Option<Value>,
+pub struct LinkedItem<Item> {
+	pub prev: Option<Item>,
+	pub next: Option<Item>,
 }
 
-pub struct LinkedList<Storage, Key, Value>(sp_std::marker::PhantomData<(Storage, Key, Value)>);
+pub struct LinkedList<Storage, Key, Item>(rstd::marker::PhantomData<(Storage, Key, Item)>);
 
 impl<Storage, Key, Value> LinkedList<Storage, Key, Value> where
-	Value: Parameter + Member + Copy,
-	Key: Parameter,
-	Storage: StorageMap<(Key, Option<Value>), LinkedItem<Value>, Query = Option<LinkedItem<Value>>>,
+  Value: Parameter + Member + Copy,
+  Key: Parameter,
+  Storage: StorageMap<(Key, Option<Value>), LinkedItem<Value>, Query = Option<LinkedItem<Value>>>,
 {
 	fn read_head(key: &Key) -> LinkedItem<Value> {
 		Self::read(key, None)
@@ -25,21 +25,56 @@ impl<Storage, Key, Value> LinkedList<Storage, Key, Value> where
 	}
 
 	fn read(key: &Key, value: Option<Value>) -> LinkedItem<Value> {
-		Storage::get((&key, value)).unwrap_or_else(|| LinkedItem {
+		Storage::get(&(key.clone(), value)).unwrap_or_else(|| LinkedItem {
 			prev: None,
 			next: None,
 		})
 	}
 
 	fn write(key: &Key, value: Option<Value>, item: LinkedItem<Value>) {
-		Storage::insert((&key, value), item);
+		Storage::insert(&(key.clone(), value), item);
 	}
 
 	pub fn append(key: &Key, value: Value) {
-        // 作业
+		let head = Self::read_head(key);
+		let new_head = LinkedItem {
+			prev: Some(value),
+			next: head.next,
+		};
+
+		Self::write_head(key, new_head);
+
+		let prev = Self::read(key, head.prev);
+		let new_prev = LinkedItem {
+			prev: prev.prev,
+			next: Some(value),
+		};
+		Self::write(key, head.prev, new_prev);
+
+		let item = LinkedItem {
+			prev: head.prev,
+			next: None,
+		};
+		Self::write(key, Some(value), item);
 	}
 
 	pub fn remove(key: &Key, value: Value) {
-        // 作业
+		if let Some(item) = Storage::take(&(key.clone(), Some(value))) {
+			let prev = Self::read(key, item.prev);
+			let new_prev = LinkedItem {
+				prev: prev.prev,
+				next: item.next,
+			};
+
+			Self::write(key, item.prev, new_prev);
+
+			let next = Self::read(key, item.next);
+			let new_next = LinkedItem {
+				prev: item.prev,
+				next: next.next,
+			};
+
+			Self::write(key, item.next, new_next);
+		}
 	}
- }
+}
